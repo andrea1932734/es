e83e878fd0fb097505e53067dbbb8e11f0bf34f5
andrea1932734 <andreacarovelli@gmail.com>
2022-01-17 12:01:45 +0000

    gg

---
+++ classi/script.js
@@ -0,0 +1,39 @@
+function startGame() {
+    myGameArea.start();
+}
+
+var myGameArea = {  
+    canvas : undefined,
+    start : function() {
+        this.canvas = document.getElementById("gameArea");
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        this.interval = setInterval(updateGameArea, 20); //ogni 20 ms chiamo il metodo updateGameArea
+    },
+
+    draw: function(component) {
+    this.context.fillStyle = component.color;
+    this.context.fillRect(component.x, component.y, component.width, component.height);
+  }
+}
+
+class Rectangle { 
+    height; 
+    width; 
+    x;
+    y;  
+    constructor(height, width,x,y){ 
+      this.height = height; 
+      this.width = width; 
+      this.x=x;
+      this.y=y; 
+    } 
+  }
+  
+  var r = new Rectangle(50,50,);
+
+
+function updateGameArea() {
+  myGameArea.draw(r);
+}
